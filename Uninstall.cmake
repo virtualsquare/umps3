3c335bd4744e6fd38012d2c60ca051c01222b754
Mattia Biondi <mattiabiondi1@gmail.com>
2020-03-25 10:23:08 +0100

    Fix libumps.e (build/install/uninstall)

--- Uninstall.cmake
+++ Uninstall.cmake
@@ -1,21 +1,22 @@
 set(MANIFEST "${CMAKE_CURRENT_BINARY_DIR}/install_manifest.txt")
 
 if(NOT EXISTS ${MANIFEST})
-    message(FATAL_ERROR "Cannot find install manifest: '${MANIFEST}'")
+        message(FATAL_ERROR "Cannot find install manifest: '${MANIFEST}'")
 endif()
 
 file(STRINGS ${MANIFEST} files)
 foreach(file ${files})
-    if(EXISTS ${file})
-        message(STATUS "Removing file: '${file}'")
+        if(EXISTS ${file} OR IS_SYMLINK ${file})
+                message(STATUS "Removing file: '${file}'")
 
-        execute_process(COMMAND rm "${file}"
-                        OUTPUT_VARIABLE remove_file)
+                execute_process(COMMAND rm "${file}"
+                                OUTPUT_VARIABLE remove_file)
 
-        if(${remove_file})
-            message(FATAL_ERROR "Failed to remove file: '${file}'.")
+                if(${remove_file})
+                        message(FATAL_ERROR
+                                "Failed to remove file: '${file}'.")
+                endif()
+        else()
+                MESSAGE(STATUS "File '${file}' does not exist.")
         endif()
-    else()
-        MESSAGE(STATUS "File '${file}' does not exist.")
-    endif()
 endforeach(file)
